a88425c62d9bb9a6a50e27109279e28cef86985f
yisufl <yisu.florida@gmail.com>
2026-01-30 22:20:36 -0500

    added verify and verify print

--- src/main.py
+++ src/main.py
@@ -50,14 +50,16 @@ def main():
     matcher = Match(n, hospital_prefs, student_prefs)
     matches = matcher.stable_matching()
 
-    verifier = Verify(matcher)
-    verifier.verify()
-    
-
-    # print results
+    # print match results
     print(str(len(matches)) + " matches made: ")
     for pair in matches:
         print(str(pair[0]) + ' ' + str(pair[1]))
+
+    verifier = Verify(n, hospital_prefs, student_prefs, matches)
+    verify_result = verifier.verify()
+
+    #print verify results
+    print("Valid Matching:", verify_result)
 
 
     # if file was given write to output file
@@ -68,12 +70,6 @@ def main():
                 file.write(str(pair[0] + 1) + ' ' + str(pair[1] + 1) + '\n') # convert back to 1-indexed
         print("Output successfully written to: " + str(out_path))
 
-
-    # create matcher and verifier
-
-    # # verify the matching
-    # is_stable = verifier.verify()
-    # print("Stable" if is_stable else "Unstable")
 
 
 if __name__ == "__main__":
--- src/verify.py
+++ src/verify.py
@@ -12,19 +12,19 @@ class Verify:
         hospitals = [h for h, _ in self.matching]
         students = [s for _, s in self.matching]
 
-        if len(set(hospitals)) != n or len(set(students)) != n:
+        if len(set(hospitals)) != self.n or len(set(students)) != self.n:
             print("hospital and student count mismatched")
             return False
 
-        if sorted(hospitals) != list(range(1, n + 1)) or sorted(students) != list(range(1, n + 1)):
+        if sorted(hospitals) != list(range(1, self.n + 1)) or sorted(students) != list(range(1, self.n + 1)):
             print("participant IDs should be 1 to n")
             return False
 
         student_to_hospital = {s: h for h, s in self.matching}
         hospital_to_student = {h: s for h, s in self.matching}
 
-        for h in range(1, n + 1):
-            for s in range(1, n + 1):
+        for h in range(1, self.n + 1):
+            for s in range(1, self.n + 1):
                 current_s = hospital_to_student[h]
                 current_h = student_to_hospital[s]
 
@@ -34,8 +34,8 @@ class Verify:
                 h_pref = self.hospital_prefs[h - 1]
                 s_pref = self.student_prefs[s - 1]
 
-                prefers_s = h_pref.index(s) < h_pref.index(current_s)
-                prefers_h = s_pref.index(h) < s_pref.index(current_h)
+                prefers_s = h_pref.index(s - 1) < h_pref.index(current_s - 1)
+                prefers_h = s_pref.index(h - 1) < s_pref.index(current_h - 1)
 
                 if prefers_s and prefers_h:
                     print("Unstable Pair:", h, "and", s)

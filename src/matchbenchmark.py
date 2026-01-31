43b2edf1a3d7aa0da89995855d2c856a64def0b2
yisufl <yisu.florida@gmail.com>
2026-01-30 23:20:13 -0500

    added separate benchmarks

--- src/matchbenchmark.py
+++ src/matchbenchmark.py
@@ -1,10 +1,11 @@
 import time
 import random
 from match import Match
+from verify import Verify
 
 def main():
     inputs = [2 ** p for p in range(1, 15)]
-
+    times = []
 
     for n in inputs:
 
@@ -14,24 +15,32 @@ def main():
         n_list = list(range(0, n))
 
         for i in range(n):
-            random.shuffle(n_list)
-            hospital_prefs.append(n_list)
+            lst = n_list.copy()
+            random.shuffle(lst)
+            hospital_prefs.append(lst)
         for i in range(n):
-            random.shuffle(n_list)
-            student_prefs.append(n_list)
+            lst = n_list.copy()
+            random.shuffle(lst)
+            student_prefs.append(lst)
 
         matcher = Match(n, hospital_prefs, student_prefs)
+        
+
 
         start_time = time.perf_counter()
-        
+         
         matcher.stable_matching()
-
+        
         end_time = time.perf_counter()
 
         elapsed_time = end_time - start_time
+        times.append(elapsed_time)
 
         print(f"Elapsed time for n = {n}: {elapsed_time}")
 
+    with open("tests_out/match_benchmark.csv", "w") as file:
+        for i in range(len(inputs)):
+            file.write(f"{inputs[i]}, {times[i]}\n")
 
 
 
---
+++ src/verifybenchmark.py
@@ -0,0 +1,49 @@
+import time
+import random
+from match import Match
+from verify import Verify
+
+def main():
+    inputs = [2 ** p for p in range(1, 15)]
+    times = []
+
+    for n in inputs:
+
+        hospital_prefs = []
+        student_prefs = []
+
+        n_list = list(range(0, n))
+
+        for i in range(n):
+            lst = n_list.copy()
+            random.shuffle(lst)
+            hospital_prefs.append(lst)
+        for i in range(n):
+            lst = n_list.copy()
+            random.shuffle(lst)
+            student_prefs.append(lst)
+
+        matcher = Match(n, hospital_prefs, student_prefs)
+        matches = matcher.stable_matching()
+        verifier = Verify(n, hospital_prefs, student_prefs, matches)
+
+
+        start_time = time.perf_counter()
+         
+        verifier.verify()
+
+        end_time = time.perf_counter()
+
+        elapsed_time = end_time - start_time
+        times.append(elapsed_time)
+
+        print(f"Elapsed time for n = {n}: {elapsed_time}")
+
+    with open("tests_out/verify_benchmark.csv", "w") as file:
+        for i in range(len(inputs)):
+            file.write(f"{inputs[i]}, {times[i]}\n")
+
+
+
+if __name__ == "__main__":
+    main()
